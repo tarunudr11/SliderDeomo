8c27f55c451300927baa9769123b8dd96bfe8d21
Tarun <tarun@baltech.in>
2015-11-05 16:22:18 +0530

    Initial Commit

---
+++ SliderDeomo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SliderDeomo
+//
+//  Created by GauravDS on 05/11/15.
+//  Copyright © 2015 baltech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
